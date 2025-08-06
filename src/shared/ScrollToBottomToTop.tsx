f8b06297fc49fe1bd6ee325fa42f5a95e6ea6993
Al Mamon <shikderr112@gmail.com>
2025-08-06 22:14:07 +0600

    fixed issue

--- src/shared/ScrollToBottomToTop.tsx
+++ src/shared/ScrollToBottomToTop.tsx
@@ -1,18 +1,29 @@
 "use client";
-import ScrollToTop from "react-scroll-up";
-
-import { IoIosArrowUp } from "react-icons/io";
+import { ChevronUp } from "lucide-react";
+import React, { useEffect, useState } from "react";
 
 const ScrollToBottomToTop = () => {
-  return (
-    <div className="relative  z-10 bg-amber-200 top-[12vh]">
-      <ScrollToTop showUnder={160}>
-        <p className="bg-primary relative -top-8 left-3  hover:primary/80 duration-300 cursor-pointer p-2 text-xl rounded  text-[#fff]">
-          <IoIosArrowUp size={18} />
-        </p>
-      </ScrollToTop>
-    </div>
-  );
+  const [visible, setVisible] = useState(false);
+  useEffect(() => {
+    const toggleVisibility = () => {
+      setVisible(window.scrollY > 300);
+    };
+
+    window.addEventListener("scroll", toggleVisibility);
+    return () => window.removeEventListener("scroll", toggleVisibility);
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+  return visible ? (
+    <button
+      onClick={scrollToTop}
+      className="fixed bottom-20 right-4 bg-black text-white px-2 py-2 border border-white/20 rounded cursor-pointer hover:bg-primary duration-300"
+    >
+      <ChevronUp size={18} />
+    </button>
+  ) : null;
 };
 
 export default ScrollToBottomToTop;
